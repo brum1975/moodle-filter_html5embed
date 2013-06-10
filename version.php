ebdc804db5d4e66bd7b19f2eec27fe4281a4a630
brum1975 <matthew.cannings@gmail.com>
2013-06-10 22:51:09 +0100

    Corrected Version.php

--- version.php
+++ version.php
@@ -25,6 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013050101;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013061100;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013050100;        // Requires this Moodle version
 $plugin->component = 'filter_html5embed'; // Full name of the plugin (used for diagnostics)
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = '0.1';
